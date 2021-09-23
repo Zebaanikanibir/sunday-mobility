cf28eb33beaae1f60cfda27475c5430bb7d5516b
Zebaanikanibir <zanikanibir@gmail.com>
2021-09-23 18:11:03 +0600

    done

---
+++ src/Component/Login/Login.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import image from '../../image/photo-1507525428034-b723cf961d3e.jpg'
+const Login = () => {
+    const [showForm, setShowForm] =useState(true)
+    const [userDetails, setUserDetails] =useState(false)
+	const userData = JSON.parse(localStorage.getItem('data'));
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    
+    const onSubmit = data => {
+        if(data===null){
+			alert('no user found!');
+		}
+			
+			
+		const same = userData.filter(d=>d.name===data.name);
+
+		console.log('same',same)
+
+			if (same[0].password===data.password){
+                alert('right password.');
+				localStorage.setItem('userlogined',data.name);
+				
+				setShowForm(false)
+			}
+			else{
+				alert('wrong password.');
+			}
+		
+
+    };
+
+  return (
+    <div className="home">
+     <div>
+     <img src={image} alt="nibir" />
+      </div>
+     <div>
+      {
+          showForm?
+         (
+           <div className="form">
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <label className="labelStyle" htmlFor="name">Name</label> <br/>
+      <input className="inputStyle" id="name" type="text" name="name"{...register('name', { required: true, maxLength: 30 })} />
+      {errors.name && errors.name.type === "required" && <span>This is required</span>}
+      {errors.name && errors.name.type === "maxLength" && <span>Max length exceeded</span> }<br/>
+      <label className="labelStyle" htmlFor="password">Password</label><br/>
+      <input className="inputStyle"  id="password" type="password" name="password"{...register('password', { required: true, maxLength: 30 })} />
+      {errors.password && errors.name.type === "required" && <span>This is required</span>}
+      {errors.password && errors.name.type === "maxLength" && <span>Max length exceeded</span> }
+
+<br/>
+
+      
+      <input className="submit" type="submit" />
+    </form>
+    <p>Have any account?</p>
+    <Link to="/register">Register</Link>
+
+      </div>   
+         ):(
+             <div className="form">
+             {
+                 userData.map(data=><li>{data.name}</li>)
+             }
+             </div>
+         )
+      }
+
+     </div>
+
+    </div>
+  )
+}
+
+export default Login
---
+++ src/Component/Register/Register.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import image from '../../image/photo-1507525428034-b723cf961d3e.jpg'
+import '../Style.css';
+const Register = () => {
+    const [userData, setUserData] = useState([]);
+	
+	
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    
+  const onSubmit = data => {
+    const newData = [...userData,data]
+    setUserData(newData)
+    
+    localStorage.setItem('data',JSON.stringify(newData));
+  console.log(true)
+    console.log('data',data)
+  };
+  return (
+    <div className="home">
+    <div >
+     <img src={image} alt="nibir" />
+      </div>
+       <div className="form"> 
+       <form  onSubmit={handleSubmit(onSubmit)}>
+      <label className="labelStyle" htmlFor="name">Name</label><br/>
+      <input className="inputStyle" id="name" type="text" name="name"{...register('name', {
+          required: true, maxLength: {
+            value: 30,
+            message: "Max length is 30"
+          },
+          pattern: {
+            value:
+
+              /^[a-zA-Z\s]*$/
+            ,
+            message: "only alphabet"
+          }
+        })} />
+      {errors.name && errors.name.type === "required" && <span style={{color:'red'}}>This is required</span>}
+      {errors.name && errors.name.type === "pattern" && (
+          <span style={{color:'red'}} role="alert">only letter</span>
+
+        )}
+<br/>
+<label className="labelStyle" htmlFor="number">Phone</label><br/>
+      <input className="inputStyle" id="number" type="text" name="number"{...register('number', {
+          required: true, maxLength: {
+            value: 10,
+            message: "Max length is 10"
+          },
+          pattern: {
+            value:
+
+            /^[0-9]*$/ 
+            ,
+            message: "only alphabet"
+          }
+        })} />
+      {errors.number && errors.number.type === "required" && <span style={{color:'red'}}>This is required</span>}
+      {errors.number && errors.number.type === "pattern" && (
+          <span role="alert">only number</span>
+
+        )}
+{errors.number && errors.number.type === "maxLength" && <span style={{color:'red'}}>Max length is 10</span> }
+<br/>
+      <label className="labelStyle" htmlFor="password">Password</label><br/>
+      <input className="inputStyle" id="password" type="password" name="password"{...register('password', {
+          required: true, maxLength: {
+            value: 30,
+            message: "Max length is 30"
+          },
+          pattern: {
+            value:
+
+            /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+            ,
+            message: "letter, number and special character. Minimum 8 characters"
+          }
+        })} />
+      {errors.password && errors.password.type === "required" && <span style={{color:'red'}}>This is required</span>}
+      {errors.password && errors.password.type === "pattern" && (
+          <span role="alert" style={{color:'red'}}>letter, number and special character. Minimum 8 characters</span>
+
+        )}
+<br/>
+      <label className="labelStyle" htmlFor="email">Email</label><br/>
+      <input className="inputStyle"  id="email" type="email" name="email"{...register('email', { required: true, maxLength: 30 })} />
+      {errors.email && errors.email.type === "required" && <span style={{color:'red'}}>This is required</span>}
+      {errors.email && errors.email.type === "maxLength" && <span style={{color:'red'}}>Max length exceeded</span> }
+<br/>
+      
+      <input className="submit" type="submit" />
+    </form>
+    <p>Already account?</p>
+    <Link to="/login">Login</Link>
+       </div>
+
+    </div>
+  )
+}
+
+export default Register
